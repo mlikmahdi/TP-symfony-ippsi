1bd935778c40bd1268816f700f4eb8da3145003c
M'LIK Mahdi <mlikmahdi1@gmail.com>
2018-02-26 16:01:44 +0100

    ajout de la liste des films et son detail

---
+++ netflix/src/AppBundle/Controller/FilmsController.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mahdi
+ * Date: 26/02/2018
+ * Time: 15:08
+ */
+
+namespace AppBundle\Controller;
+
+
+use AppBundle\Entity\Films;
+use AppBundle\Manager\FilmsManager;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FilmsController extends Controller
+{
+    /**
+     * @Route("/films", name="films_list") */
+    public function listFilmsAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $films = $em->getRepository(Films:: class)->findAll();
+        return $this->render('default/films/list.html.twig', [
+            'films' => $films ]);
+    }
+
+    /**
+     * @Route("/films/{id}", name="film_view", requirements={"id"="\d+"})
+     */
+    public function viewFilmAction(int $id, FilmsManager $filmsManager)
+    {
+        $film = $filmsManager->getFilm($id);
+        return $this->render('default/films/view.html.twig',
+            [ 'film' => $film ]);
+    }
+}
---
+++ netflix/src/AppBundle/Manager/FilmsManager.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mahdi
+ * Date: 26/02/2018
+ * Time: 15:22
+ */
+
+namespace AppBundle\Manager;
+
+
+use AppBundle\Entity\Films;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class FilmsManager
+{
+
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->em = $entityManager;
+    }
+
+    public function getFilms()
+    {
+        return $this->em->getRepository(Films::class)->findAll();
+    }
+
+    public function getFilm(int $id)
+    {
+        $film = $this->em->getRepository(Films::class)->find($id);
+        if (!$film) {
+            $this->errorAction(404);
+        }
+        return $film;
+    }
+
+    /**
+     *  Retourner une erreur
+     */
+    private function errorAction($error)
+    {
+        switch ($error) {
+            case 404:
+                throw new NotFoundHttpException('404, Article not found');
+                break;
+            case 400:
+                throw new BadRequestHttpException('400, Bad request');
+                break;
+            default:
+                throw new HttpException($error, 'Article error');
+                break;
+        }
+
+    }
+}
